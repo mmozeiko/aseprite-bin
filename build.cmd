eb5f0f8398a3491db273d392ea3d4e65eb7991fe
Martins Mozeiko <martins.mozeiko@gmail.com>
2022-06-01 19:15:45 -0700

    update to vs2022

--- build.cmd
+++ build.cmd
@@ -1,15 +1,39 @@
 @echo off
-setlocal 
+setlocal enabledelayedexpansion
 
 set PATH="C:\Program Files\7-Zip";%PATH%
 
-echo fetching latest release version
+rem *** Visual Studio environment ***
+
+where /Q cl.exe || (
+  set __VSCMD_ARG_NO_LOGO=1
+  for /f "tokens=*" %%i in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath') do set VS=%%i
+  if "!VS!" equ "" (
+    echo ERROR: Visual Studio installation not found
+    exit /b 1
+  )  
+  call "!VS!\VC\Auxiliary\Build\vcvarsall.bat" amd64 || exit /b 1
+)
+
+
+rem *** ninja
+
+where /q ninja.exe || (
+  curl -LOsf https://github.com/ninja-build/ninja/releases/download/v1.11.0/ninja-win.zip || exit /b 1
+  7z x -bb0 -y ninja-win.zip 1>nul 2>nul || exit /b 1
+  del ninja-win.zip 1>nul 2>nul
+)
+
+
+rem *** fetching latest release version
+
 for /F "delims=" %%v in ('"curl -sfL https://api.github.com/repos/aseprite/aseprite/releases/latest | jq .tag_name -r"') do (
   set ASEPRITE_VERSION=%%v
 )
 
 
-echo checking if release is already built
+rem **** checking if release is already built
+
 curl -sfLo nul https://api.github.com/repos/mmozeiko/aseprite-bin/releases/tags/%ASEPRITE_VERSION%
 if %ERRORLEVEL% EQU 0 (
   echo release already exists, exiting
@@ -17,24 +41,26 @@ if %ERRORLEVEL% EQU 0 (
 )
 
 
-echo cloning asesprite %ASEPRITE_VERSION%
+rem **** cloning asesprite repo
+
 git clone --quiet -c advice.detachedHead=false --no-tags --recursive --depth=1 -b "%ASEPRITE_VERSION%" https://github.com/aseprite/aseprite.git || echo "failed to clone repo" && exit /b
 python -c "v = open('aseprite/src/ver/CMakeLists.txt').read(); open('aseprite/src/ver/CMakeLists.txt', 'w').write(v.replace('1.x-dev', '%ASEPRITE_VERSION%'[1:]))"
 
 
-echo downloading skia
+rem *** downloading skia
+
 mkdir skia
 cd skia
-curl -sfLO https://github.com/aseprite/skia/releases/download/m96-2f1f21b8a9/Skia-Windows-Release-x64.zip || echo failed to download skia && exit /b
+curl -sfLO https://github.com/aseprite/skia/releases/download/m102-861e4743af/Skia-Windows-Release-x64.zip || echo failed to download skia && exit /b 1
 7z x -y Skia-Windows-Release-x64.zip
 cd ..
 
 
-echo building asesprite
+rem *** building asesprite
+
 set LINK=opengl32.lib
 cmake                                          ^
-  -G "Visual Studio 16 2019"                   ^
-  -A x64                                       ^
+  -G Ninja                                     ^
   -S aseprite                                  ^
   -B build                                     ^
   -DCMAKE_BUILD_TYPE=Release                   ^
@@ -43,13 +69,12 @@ cmake                                          ^
   -DLAF_BACKEND=skia                           ^
   -DSKIA_DIR=%CD%\skia                         ^
   -DSKIA_LIBRARY_DIR=%CD%\skia\out\Release-x64 ^
-  -DSKIA_OPENGL_LIBRARY=                        || echo failed to configure build && exit /b
-python -c "v = open('build/src/aseprite.vcxproj').read(); open('build/src/aseprite.vcxproj', 'w').write(v.replace(';..\lib\..\libcurl.lib;', ';..\lib\libcurl.lib;'))"
-python -c "v = open('build/src/aseprite.vcxproj').read(); open('build/src/aseprite.vcxproj', 'w').write(v.replace(';.\lib\libcurl.lib;', ';..\lib\libcurl.lib;'))"
-cmake --build build --config Release -- -m -v:q || echo "build failed" && exit /b
+  -DSKIA_OPENGL_LIBRARY=                        || echo failed to configure build && exit /b 1
+ninja -C build || echo "build failed" || exit /b 1
 
 
-echo creating zip file
+rem *** creating zip file
+
 mkdir aseprite-%ASEPRITE_VERSION%
 echo # This file is here so Aseprite behaves as a portable program >aseprite-%ASEPRITE_VERSION%\aseprite.ini
 xcopy /E /Q /Y aseprite\docs aseprite-%ASEPRITE_VERSION%\docs\
